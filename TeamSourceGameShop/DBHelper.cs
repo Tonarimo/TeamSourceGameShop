6c4afb0c7e210ffedd12c20c1d1ec56f00d4e2bd
Eli Artz <Artz0801@student.cptc.edu>
2018-02-07 12:59:01 -0800

    Added A database project to the current project and a DBHelper class that will help other methods and classes to connect to the database.

---
+++ TeamSourceGameShop/DBHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSourceGameShop
+{
+    class DBHelper
+    {
+        /// <summary>
+        /// Returns the connection string to the method that calls it.
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=GameStoreDatabase;Integrated Security=True");
+        }
+    }
+}
